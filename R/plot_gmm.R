d0d7ce4f96d6c1e6c0e34c1a4f0bfe525a07bdf8
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2020-07-07 16:40:35 -0500

    Update and rename plot_GMM.R to plot_gmm.R

--- R/plot_gmm.R
+++ R/plot_gmm.R
@@ -1,30 +1,25 @@
 #' Plots Mixture Components from Gaussian Mixture Models
-#' 
+#'
 #' Generates a plot of data densities with overlaid mixture components from a Gaussian mixture model (GMM)
-#' @usage plot_GMM(m, k=NULL)
+#' @usage plot_gmm(m, k = NULL)
 #' @param m An object of class \code{mixEM} corresponding with the fit GMM
 #' @param k The number of components specified in the GMM, \code{m}
-#' @details Uses ggplot2 graphics to plot data densities with overlaid components from \code{mixEM} objects, which are GMM's fit using the \code{mixtools} package.
-#' 
-#' Note: Users must enter the same component value, \code{k}, in the \code{plot_GMM} function, as that which was specified in the original GMM specification (also \code{k} in \code{mixtools}).
+#' @details Original function from the \code{plotGMM} package. Retained here for bridging between the packages. We recommend using instead the updated \code{plot_mm} function.
+#'
+#' Note: \code{plot_gmm} requires a \code{mixtools} object to be supplied. Users must enter the same component value, \code{k}, in the \code{plot_gmm} function, as that which was specified in the original GMM specification (also \code{k} in \code{mixtools}).
 #' @examples
 #' set.seed(235)
 #' mixmdl <- mixtools::normalmixEM(faithful$waiting, k = 2)
-#' 
-#' plot_GMM(mixmdl, 2)
+#'
+#' plot_gmm(mixmdl, 2)
 #' @references Benaglia, T., Chauveau, D., Hunter, D. and Young, D., 2009. mixtools: An R package for analyzing finite mixture models. Journal of Statistical Software, 32(6), pp.1-29.
-#' 
+#'
 #' Wickham, H., 2016. ggplot2: elegant graphics for data analysis. Springer.
-#' 
+#'
 #' @export
-
-plot_GMM <- function(m, k = NULL) {
+plot_gmm <- function(m, k=NULL) {
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("Package \"ggplot2\" needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
-  if (!requireNamespace("mixtools", quietly = TRUE)) {
-    stop("Package \"mixtools\" needed for this function to work. Please install it.",
          call. = FALSE)
   }
   if (!requireNamespace("methods", quietly = TRUE)) {
@@ -40,7 +35,7 @@ plot_GMM <- function(m, k = NULL) {
   if (k <= 1){
     stop("Specified components must be at least length 2.")
   } else if (k >= 16){
-    stop("Are you really searching for 16 or more components? If so, open an issue ticket: `https://github.com/pdwaggoner/plotGMM/issues`\nand we will consider updating the package. If not, go back and make sure the GMM is properly specified.")
+    stop("Are you really searching for 16 or more clusters? If so, open an issue ticket: `https://github.com/pdwaggoner/plotGMM/issues`\nand we will consider updating the package. If not, go back and make sure the GMM is properly specified.")
   }
 
   component_colors <-
@@ -53,10 +48,10 @@ plot_GMM <- function(m, k = NULL) {
       "purple",
       "darksalmon",
       "goldenrod2",
-      "dodgerblue", 
+      "dodgerblue",
       "darkorange3",
-      "burlywood4", 
-      "darkmagenta", 
+      "burlywood4",
+      "darkmagenta",
       "firebrick",
       "deeppink2",
       "darkseagreen1"
@@ -70,8 +65,8 @@ plot_GMM <- function(m, k = NULL) {
     )
 
   for (i in seq(1, k)) {
-    out_plot <- 
-      out_plot + 
+    out_plot <-
+      out_plot +
       ggplot2::stat_function(geom = "line", fun = plot_mix_comps,
                            args = list(m$mu[i], m$sigma[i], lam = m$lambda[i]),
                            colour = component_colors[i], lwd = 1) +
@@ -80,5 +75,5 @@ plot_GMM <- function(m, k = NULL) {
   }
 
   out_plot
-  
+
 }
