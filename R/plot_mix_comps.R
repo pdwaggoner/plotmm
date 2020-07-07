4e737b592dd352b2e0de33b26151e4620474ad87
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2020-07-07 16:42:41 -0500

    Update plot_mix_comps.R

--- R/plot_mix_comps.R
+++ R/plot_mix_comps.R
@@ -1,26 +1,63 @@
-#' Custom Function for Overlaying Mixture Components
+#' Helper Function for Overlaying Mixture Components
 #'
-#' Plots a mixture component conditioned on a superimposed function
-#' @usage plot_mix_comps(x, mu, sigma, lam)
+#' Allows for plotting mixture components conditioned on a superimposed function meant for passage to ggplot's \code{stat_function()}
+#' @usage plot_mix_comps(x, mu = NULL, sigma = NULL, lam = 1, beta0 = NULL,
+#'  beta1=NULL, alpha=NULL, beta=NULL,
+#'  normal=FALSE, logisreg=FALSE,
+#'  gamma=FALSE, poisson=FALSE)
 #' @param x Input data
-#' @param mu Mean of component
-#' @param sigma Variance of component
-#' @param lam Mixture weight of component
-#' @details Allows for specifying a custom function to be superimposed when plotting a mixture component
+#' @param mu Component mean
+#' @param sigma Component variance
+#' @param lam Component mixture weight
+#' @param alpha Initial shape parameters
+#' @param beta Initial parameter values
+#' @param beta0 Coefficient values
+#' @param beta1 Coefficient values
+#' @param normal Logical for normal distribution
+#' @param logisreg Logical for logistic regression mixtures
+#' @param gamma Logical for gamma distribution
+#' @param poisson Logical for poisson regression mixtures
+#' @details Allows for component curves to be superimposed over a mixture model plot
 #' @examples
+#' \dontrun{
 #' set.seed(1)
 #' mixmdl <- mixtools::normalmixEM(faithful$waiting, k = 2)
 #' x <- mixmdl$x
 #' x <- data.frame(x)
 #' ggplot2::ggplot(data.frame(x)) +
-#'   ggplot2::geom_histogram(ggplot2::aes(x, ..density..)) +
+#'   ggplot2::geom_density(ggplot2::aes(x), color="black", fill="black") +
 #'   ggplot2::stat_function(geom = "line", fun = plot_mix_comps,
 #'                 args = list(mixmdl$mu[1], mixmdl$sigma[1], lam = mixmdl$lambda[1]),
 #'                 colour = "red") +
 #'   ggplot2::stat_function(geom = "line", fun = plot_mix_comps,
 #'                 args = list(mixmdl$mu[2], mixmdl$sigma[2], lam = mixmdl$lambda[2]),
 #'                colour = "blue")
+#'}
+#'
 #' @export
-plot_mix_comps <- function(x, mu, sigma, lam) {
-  lam * stats::dnorm(x, mu, sigma)
+plot_mix_comps <- function(x, mu = NULL, sigma = NULL, lam = 1, beta0 = NULL,
+                           beta1 = NULL, alpha = NULL, beta = NULL,
+                           normal = FALSE, logisreg = FALSE,
+                           gamma = FALSE, poisson = FALSE) {
+  if (normal==TRUE) {
+    if(is.null(mu) | is.null(sigma) | is.null(lam)){
+      stop("Check input for normal distribution.")
+    }
+    lam*stats::dnorm(x, mean = mu, sd = sigma)
+  } else if (logisreg==TRUE) {
+    if(is.null(beta0) | is.null(beta1)){
+      stop("Check input for binary logistic regrssions.")
+    }
+    stats::plogis(beta0 + beta1*x)
+  } else if (gamma==TRUE) {
+    if(is.null(alpha) | is.null(beta) | is.null(lam)){
+      stop("Check input for gamma distribution.")
+    }
+    lam*stats::dgamma(x, shape = alpha, scale = beta)
+  } else if (poisson==TRUE) {
+    if(is.null(beta0) | is.null(beta1)){
+      stop("Check input for poisson distribution.")
+    }
+    lam*stats::rpois(x, exp(beta0 + beta1))
+  }
 }
