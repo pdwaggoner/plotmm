392462ce1ac0d4ab47431280685ce5ed971dbf7c
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2020-07-08 10:03:10 -0500

    Update plot_mm.R

--- R/plot_mm.R
+++ R/plot_mm.R
@@ -3,18 +3,22 @@
 #' Generates a ggplot of data densities with overlaid mixture components from fit mixture models.
 #' @usage plot_mm(m, k = NULL, data = NULL)
 #' @param m A mixture model object
-#' @param k The number of components specified in the mixture model, \code{m}
+#' @param k Optional. The number of components specified in the mixture model, \code{m}
 #' @param data Name of data object required only for \code{EMCluster} objects
+#'
 #' @details This is the core function in the package, returning a \code{ggplot} object for a fit mixture model. The plot includes the data density with overlaid mixture components.
 #'
 #' @examples
-#' \dontrun{
-#' set.seed(235)
+#' \donttest{
+#' if(require(mixtools)){
 #' mixmdl1 <- mixtools::normalmixEM(faithful$waiting, k = 2)
+#' }
 #' plot_mm(mixmdl1, 2)
 #'
+#' if(require(mixtools)){
 #' x <- c(rgamma(200, shape = 50, scale = 11), rgamma(200, shape = 28, scale = 6))
 #' mixmdl2 <- mixtools::gammamixEM(x, lambda = c(1, 1)/2)
+#' }
 #' plot_mm(mixmdl2)
 #'}
 #'
