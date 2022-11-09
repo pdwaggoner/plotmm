c6628fc14a132d953517314f420406e58f8e7100
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2022-11-09 10:20:22 -0700

    Update plot_mix_comps_normal.R

--- R/plot_mix_comps_normal.R
+++ R/plot_mix_comps_normal.R
@@ -10,21 +10,21 @@
 #' @details Allows for specifying a custom function to be superimposed when plotting a mixture component assuming a normal distribution. This is the original function for the package, which is also included in the updated \code{plot_mix_comps()} function.
 #'
 #' @examples
+#' \donttest{
 #' if(require(mixtools)){
 #' mixmdl <- mixtools::normalmixEM(faithful$waiting, k = 2)
 #' }
 #' x <- mixmdl$x
 #' x <- data.frame(x)
 #' ggplot2::ggplot(data.frame(x)) +
-#' ggplot2::geom_histogram(ggplot2::aes(x, ..density..), binwidth = 1, colour = "black", fill = "white") +
-#' ggplot2::stat_function(geom = "line", fun = plotmm::plot_mix_comps_normal,
-#'                        args = list(mu = mixmdl$mu[1], sigma = mixmdl$sigma[1], lam = mixmdl$lambda[1]),
-#'                        colour = "red", lwd = 1) +
-#' ggplot2::stat_function(geom = "line", fun = plotmm::plot_mix_comps_normal,
-#'                        args = list(mu = mixmdl$mu[2], sigma = mixmdl$sigma[2], lam = mixmdl$lambda[2]),
-#'                        colour = "blue", lwd = 1) +
-#' ggplot2::ylab("Density") + 
-#' ggplot2::theme_minimal()
+#'   ggplot2::geom_density(ggplot2::aes(x), color="black", fill="black") +
+#'   ggplot2::stat_function(geom = "line", fun = plot_mix_comps_normal,
+#'                 args = list(mixmdl$mu[1], mixmdl$sigma[1], lam = mixmdl$lambda[1]),
+#'                 colour = "red") +
+#'   ggplot2::stat_function(geom = "line", fun = plot_mix_comps_normal,
+#'                 args = list(mixmdl$mu[2], mixmdl$sigma[2], lam = mixmdl$lambda[2]),
+#'                colour = "blue")
+#'}
 #'
 #' @export
 plot_mix_comps_normal <- function(x, mu, sigma, lam) {
